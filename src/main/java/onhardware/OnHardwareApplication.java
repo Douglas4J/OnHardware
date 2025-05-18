5a4e78db3191c5a9d4517258dda6a0c5ae2ef7c1
Douglas4J <douglas.ferrerira@onpagtech.com.br>
2025-05-17 21:00:25 -0300

    Criação do Projeto

---
+++ src/main/java/onhardware/OnHardwareApplication.java
@@ -0,0 +1,11 @@
+package onhardware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnHardwareApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OnHardwareApplication.class, args);
+	}
+}
---
+++ src/main/java/onhardware/controller/TesteController.java
@@ -0,0 +1,13 @@
+package onhardware.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/")
+    public String testePage() {
+        return "PAGINA DE TESTE FUNCIONANDO";
+    }
+}
