921d9a652b24f8475d2a0fbc2fc79cfc7162ba42
Douglas4J <douglas.ferrerira@onpagtech.com.br>
2025-05-19 11:42:36 -0300

    Tratando DTO

--- src/main/java/onhardware/DTO/ProdutoDTO.java
+++ src/main/java/onhardware/DTO/ProdutoDTO.java
@@ -1,4 +1,32 @@
 package onhardware.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProdutoDTO {
+
+    private Long idProduto;
+
+    @NotBlank(message = "O nome do produto é obrigatório.")
+    private String nomeProduto;
+
+    @NotBlank(message = "A marca do produto é obrigatória.")
+    private String marcaProduto;
+
+    @NotBlank(message = "O modelo do produto é obrigatório.")
+    private String modeloProduto;
+
+    @NotBlank(message = "As especificações do produto são obrigatórias.")
+    @Size(max = 300, message = "As especificações deve ter no máximo 300 caracteres.")
+    private String especificacaoProduto;
+
+    @NotNull(message = "O preço do produto é obrigatório.")
+    @Positive(message = "O preço do produto deve ser maior que zero.")
+    private BigDecimal precoProduto;
 }
--- src/main/java/onhardware/model/Produto.java
+++ src/main/java/onhardware/model/Produto.java
@@ -2,10 +2,6 @@ package onhardware.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -14,31 +10,25 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name = "produtos")
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idProduto;
 
-    @NotBlank(message = "O nome do produto é obrigatório.")
     @Column(nullable = false)
     private String nomeProduto;
 
-    @NotBlank(message = "A marca do produto é obrigatória.")
     @Column(nullable = false)
     private String marcaProduto;
 
-    @NotBlank(message = "O modelo do produto é obrigatório.")
     @Column(nullable = false)
     private String modeloProduto;
 
-    @NotBlank(message = "As especificações do produto são obrigatórias.")
-    @Size(max = 300, message = "As especificações deve ter no máximo 300 caracteres.")
-    @Column(nullable = false)
+    @Column(nullable = false, length = 300)
     private String especificacaoProduto;
 
-    @NotNull(message = "O preço do produto é obrigatório.")
-    @Positive(message = "O preço do produto deve ser maior que zero.")
     @Column(nullable = false)
     private BigDecimal precoProduto;
 
@@ -46,5 +36,4 @@ public class Produto {
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     @Column(nullable = false, updatable = false)
     private LocalDateTime dataRegistroProduto;
-
 }
